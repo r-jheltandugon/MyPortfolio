a22c936b09a4a67b0e56405681cde31b69baa28d
ambo18 <jherietandugon@gmail.com>
2024-08-22 15:25:35 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Projects from "./pages/Projects";
+// import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 
 const App = () => {
--- src/data.tsx
+++ src/data.tsx
@@ -2,13 +2,13 @@ import {
   SiReact,
   SiTypescript,
   SiJavascript,
-  SiExpress,
-  SiMongodb,
+  // SiExpress,
+  // SiMongodb,
   SiPostgresql,
-  SiAngular,
-  SiNextdotjs,
-  SiNestjs,
-  SiMicrosoftsqlserver,
+  // SiAngular,
+  // SiNextdotjs,
+  // SiNestjs,
+  // SiMicrosoftsqlserver,
   SiBootstrap,
   SiFirebase,
   SiDjango,
@@ -16,76 +16,76 @@ import {
   SiPhp,
   SiMysql,
 } from "react-icons/si";
-import dora from "./assets/dora.png";
-import trinketsofluna from "./assets/trinketsofluna.png";
-import weatherapp from "./assets/weatherapp.png";
-import spotify from "./assets/spotify.png";
-import anime from "./assets/anime.png";
-import voting from "./assets/voting.png";
+// import dora from "./assets/dora.png";
+// import trinketsofluna from "./assets/trinketsofluna.png";
+// import weatherapp from "./assets/weatherapp.png";
+// import spotify from "./assets/spotify.png";
+// import anime from "./assets/anime.png";
+// import voting from "./assets/voting.png";
 
-export const projects = [
-  {
-    id: 1,
-    image: dora,
-    name: "Disaster Online Reporting Application V4",
-    description:
-      "Android-based mobile application that allows users to easily and quickly report disasters in real-time.",
-    badges: ["Java", "Firebase", "Android", "Google Maps"],
-    url: "https://github.com/ChickenCombo/disaster-online-reporting-application",
-  },
-  {
-    id: 2,
-    image: trinketsofluna,
-    name: "Ecommerce Web App for Trinkets of Luna",
-    description:
-      "An Ecommerce web app with ordering system, inventory system, and payment system.",
-    badges: ["React.js", "Firebase", "PayMongo"],
-    url: "https://github.com/cedricquitor/trinketsofluna-ecommerce-web-app",
-  },
-  {
-    id: 3,
-    image: voting,
-    name: "Blockchain Voting dApp",
-    description:
-      "A blockchain-based electoral college voting dApp project on the Ethereum blockchain.",
-    badges: [
-      "React.js",
-      "TypeScript",
-      "Hardhat",
-      "Solidity",
-      "TailwindCSS",
-      "Web3",
-    ],
-    url: "https://github.com/cedricquitor/blockchain-web-app",
-  },
-  {
-    id: 4,
-    image: spotify,
-    name: "Higher or Lower: Spotify",
-    description:
-      "A simple game which players try to guess whether the next artist has higher or lower monthly listeners than the current artist.",
-    badges: ["React.js", "TypeScript", "TailwindCSS", "Puppeteer"],
-    url: "https://github.com/ChickenCombo/higher-or-lower-spotify",
-  },
-  {
-    id: 5,
-    image: anime,
-    name: "Higher or Lower: Anime",
-    description:
-      "A simple game which players try to guess whether the next anime has higher or lower rating than the current anime.",
-    badges: ["Vanilla JavaScript", "HTML", "CSS"],
-    url: "https://github.com/ChickenCombo/higher-or-lower-anime",
-  },
-  {
-    id: 6,
-    image: weatherapp,
-    name: "Weather App",
-    description:
-      "Android-based Weather application to quickly and accurately check the weather anywhere.",
-    badges: ["Java", "Android", "OpenWeather", "Geocoding API"],
-    url: "https://github.com/ChickenCombo/weather-app",
-  },
-];
+// export const projects = [
+//   {
+//     id: 1,
+//     image: dora,
+//     name: "Disaster Online Reporting Application V4",
+//     description:
+//       "Android-based mobile application that allows users to easily and quickly report disasters in real-time.",
+//     badges: ["Java", "Firebase", "Android", "Google Maps"],
+//     url: "https://github.com/ChickenCombo/disaster-online-reporting-application",
+//   },
+//   {
+//     id: 2,
+//     image: trinketsofluna,
+//     name: "Ecommerce Web App for Trinkets of Luna",
+//     description:
+//       "An Ecommerce web app with ordering system, inventory system, and payment system.",
+//     badges: ["React.js", "Firebase", "PayMongo"],
+//     url: "https://github.com/cedricquitor/trinketsofluna-ecommerce-web-app",
+//   },
+//   {
+//     id: 3,
+//     image: voting,
+//     name: "Blockchain Voting dApp",
+//     description:
+//       "A blockchain-based electoral college voting dApp project on the Ethereum blockchain.",
+//     badges: [
+//       "React.js",
+//       "TypeScript",
+//       "Hardhat",
+//       "Solidity",
+//       "TailwindCSS",
+//       "Web3",
+//     ],
+//     url: "https://github.com/cedricquitor/blockchain-web-app",
+//   },
+//   {
+//     id: 4,
+//     image: spotify,
+//     name: "Higher or Lower: Spotify",
+//     description:
+//       "A simple game which players try to guess whether the next artist has higher or lower monthly listeners than the current artist.",
+//     badges: ["React.js", "TypeScript", "TailwindCSS", "Puppeteer"],
+//     url: "https://github.com/ChickenCombo/higher-or-lower-spotify",
+//   },
+//   {
+//     id: 5,
+//     image: anime,
+//     name: "Higher or Lower: Anime",
+//     description:
+//       "A simple game which players try to guess whether the next anime has higher or lower rating than the current anime.",
+//     badges: ["Vanilla JavaScript", "HTML", "CSS"],
+//     url: "https://github.com/ChickenCombo/higher-or-lower-anime",
+//   },
+//   {
+//     id: 6,
+//     image: weatherapp,
+//     name: "Weather App",
+//     description:
+//       "Android-based Weather application to quickly and accurately check the weather anywhere.",
+//     badges: ["Java", "Android", "OpenWeather", "Geocoding API"],
+//     url: "https://github.com/ChickenCombo/weather-app",
+//   },
+// ];
 
 export const programmingLanguages = [
   { name: "TypeScript", icon: <SiTypescript /> },
